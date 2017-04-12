91158d3baac8b18bc10d5bb1807ff72e85299118
Radu Achim <a8a3d2@yahoo.com>
2017-04-12 11:31:50 +0200

    Update counter.component.ts

--- ClientApp/app/components/counter/counter.component.ts
+++ ClientApp/app/components/counter/counter.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
     selector: 'counter',
@@ -8,6 +9,15 @@ export class CounterComponent {
     public currentCount = 0;
 
     public incrementCounter() {
+        this
+            .http
+            .post('http://localhost:5000/home/insertsubscriber', {
+                email: 'ram@iteam.se',
+                name: 'RAM'
+            })
+        .subscribe(result => console.log(result))
         this.currentCount++;
     }
+
+    constructor(private http: Http) {}
 }
